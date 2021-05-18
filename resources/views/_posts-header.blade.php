d9ea4c53359f6d7fcf0b20a9c73260b06fbd0c16
pouyabh <pouyabh1380@gmail.com>
2021-05-18 09:19:10 +0430

    First Step To Build Search With Query

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -27,29 +27,30 @@
         </div>
 
         <!-- Other Filters -->
-{{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
-{{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
-{{--                <option value="category" disabled selected>Other Filters</option>--}}
-{{--                <option value="foo">Foo</option>--}}
-{{--                <option value="bar">Bar</option>--}}
-{{--            </select>--}}
+    {{--        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">--}}
+    {{--            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">--}}
+    {{--                <option value="category" disabled selected>Other Filters</option>--}}
+    {{--                <option value="foo">Foo</option>--}}
+    {{--                <option value="bar">Bar</option>--}}
+    {{--            </select>--}}
 
-{{--            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"--}}
-{{--                 height="22" viewBox="0 0 22 22">--}}
-{{--                <g fill="none" fill-rule="evenodd">--}}
-{{--                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
-{{--                    </path>--}}
-{{--                    <path fill="#222"--}}
-{{--                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>--}}
-{{--                </g>--}}
-{{--            </svg>--}}
-{{--        </div>--}}
+    {{--            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"--}}
+    {{--                 height="22" viewBox="0 0 22 22">--}}
+    {{--                <g fill="none" fill-rule="evenodd">--}}
+    {{--                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">--}}
+    {{--                    </path>--}}
+    {{--                    <path fill="#222"--}}
+    {{--                          d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>--}}
+    {{--                </g>--}}
+    {{--            </svg>--}}
+    {{--        </div>--}}
 
-        <!-- Search -->
+    <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
                 <input type="text" name="search" placeholder="Find something"
-                       class="bg-transparent placeholder-black font-semibold text-sm">
+                       class="bg-transparent placeholder-black font-semibold text-sm"
+                       value="{{request('search')}}">
             </form>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%' . request('search') . '%')
+            ->orWhere('body', 'like', '%' . request('search') . '%');
+    }
     return view('posts', [
-        'posts' => Post::latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
