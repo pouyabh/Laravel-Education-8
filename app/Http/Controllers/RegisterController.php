0667bd256f5e4fb1fcf14771c2d6738f51bad636
pouyabh <pouyabh1380@gmail.com>
2021-06-07 10:21:06 +0430

    Make A user and validate date

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'name' => 'required|max:50|min:2',
+            'username' => 'required|max:50|min:2',
+            'email' => 'required|max:50|min:2',
+            'password' => 'required|max:50|min:2'
+        ]);
+
+        User::create($attributes);
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
