eb90bc047dddd81ca154ad6a1bd4eee550de91b1
pouyabh <pouyabh1380@gmail.com>
2021-06-12 10:23:07 +0430

    Build Login PG And Episode 50 Complete

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,13 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            session()->regenerate();
+
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        /*
+                return back()
+                    ->withInput()
+                    ->withErrors(['email', 'Your provided crendentials could not verified.']);
+        */
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified.'
+        ]);
+    }
+
     public function destroy()
     {
         auth()->logout();
-        return redirect('/')->with('success', 'GoodBy 👋');
+
+        return redirect('/')->with('success', 'Goodbye!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::post('logout', [SessionController::class, 'destroy']);
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('login', [SessionController::class, 'store'])->middleware('guest');;
+
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 
