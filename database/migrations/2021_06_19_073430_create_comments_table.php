2c867a15b59ddf3b6b31fdf3030f2516e6d45981
pouyabh <pouyabh1380@gmail.com>
2021-06-19 12:06:22 +0430

    Cacade Comment Ep 53 complited

--- database/migrations/2021_06_19_073430_create_comments_table.php
+++ database/migrations/2021_06_19_073430_create_comments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateCommentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,11 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('slug')->unique();
-            $table->string('title');
-            $table->text('excerpt');
-            $table->text('body');
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
-            $table->timestamp('published_at')->nullable();
         });
     }
 
@@ -33,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 }
