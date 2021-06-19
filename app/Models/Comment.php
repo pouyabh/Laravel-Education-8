ba97a8ce9c3afaf4a322b27a83a402f730aa87d6
pouyabh <pouyabh1380@gmail.com>
2021-06-19 14:03:42 +0430

    Show Comment with Uniq avatar Ep 53 complited

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
--- database/migrations/2021_06_19_073430_create_comments_table.php
+++ database/migrations/2021_06_19_073430_create_comments_table.php
@@ -17,6 +17,7 @@ class CreateCommentsTable extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->text('body');
             $table->timestamps();
         });
     }
